dc6a60f0cc41b4c9c52455f62c9ddfc5cd1b9f5d
nandatnl <fernanda@poppymidia.com.br>
2023-03-02 07:55:09 -0300

    Inserindo classe pessoa

--- fundamentos_oo/src/main/kotlin/one/digitalInnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalInnovation/digionebank/Pessoa.kt
@@ -3,12 +3,13 @@ package one.digitalInnovation.digionebank
 class Pessoa {
     var nome:String ="Fernanda"
     var cpf: String ="123.345.989-11"
-    inner class Endereco {
-        var rua : String = "Rua teste"
-    }
+    private set
+    constructor()
+    fun pessoaInfo()="$nome e $cpf"
 }
 fun main(){
     val fernanda = Pessoa()
-    println(fernanda.nome)
-    println(fernanda.cpf)
+
+    println(fernanda.pessoaInfo())
+
 }
